7e8a41a687e9a5059bd3afe77f9a4b9456b6d292
Alex Pascoe <AlexPascoe@Alexs-MacBook-Air.local>
2017-11-30 20:07:23 +0400

    mod 4 assign update

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -60,7 +60,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
     if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
+      byeSpeaker.speak(names[i]);
     } else {
       helloSpeaker.speak(names[i]);
     }
